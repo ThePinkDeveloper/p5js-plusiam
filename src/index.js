050c9f34f0c590e2fca1232b74a8d94422cf3d17
Miguel Rosa <nmlbstudio@gmail.com>
2022-02-25 01:37:18 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const BASE_WIDTH = 412;
+const BASE_HEIGHT = 780;
+
+function setup() {
+    var canvas = createCanvas(BASE_WIDTH, BASE_HEIGHT);
+    canvas.parent('main');
+    background(255, 0, 200);   
+}
+
+function draw() {
+
+}
+
+
